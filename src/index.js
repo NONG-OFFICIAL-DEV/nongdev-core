379361188724b819150ab2c8d769cf4477d10b84
ONG-PHLOEUT-GITHUB <phloeutnong@gmail.com>
2026-03-01 22:26:50 +0700

    include plugins and composables in package

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// API
+export { menuApi } from "./api/menu";
+export { tableApi } from "./api/table";
+export { authApi } from "./api/auth";
+
+export { CorePlugin } from "./plugin";
+
+// Utils
+export {
+  formatCurrency,
+  formatCurrencyNoSymbol,
+  formatKHR,
+} from "./utils/currency";
+export {
+  formatDate,
+  formatDateTime,
+  formatDateText,
+  formatLocalDate,
+  formatTimeAgo,
+  addDays,
+} from "./utils/date";
+
+export {
+  calcTotal,
+  calcItemCount,
+  ORDER_STATUS,
+  KITCHEN_STATUS,
+} from "./utils/order";
+
+// Echo
+export { useEcho, createEcho } from "../echo";
